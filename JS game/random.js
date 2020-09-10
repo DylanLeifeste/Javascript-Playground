f79d6eba57047212cf50c0000aa0508f12d94b60
Dylan Leifeste <ldylan01@gmail.com>
2020-09-09 20:36:27 -0500

    work backwards/ counter and button func work

--- JS game/random.js
+++ JS game/random.js
@@ -2,11 +2,23 @@
 
 alert("a game in progress with much more work to come");
 
-document.getElementById("image").onclick = function() {
-    if(this.src === "peter-punch.png"){
-        this.src = "peter-punched.png";
-    } else {
-        this.src = "peter-punch.png";
-    }
-};
+// document.getElementById("image").onclick = function() {
+//     if(this.src === "peter-punch.png"){
+//         this.src = "peter-punched.png";
+//     } else {
+//         this.src = "peter-punch.png";
+//     }
+// };
+
+var x=0;
+var lv=1;
+function punch(){
+    x+=lv;
+    textUpdate();
+
+}
+function textUpdate(){
+    document.getElementById("punches").innerHTML=x+" Punches";
+
+}
 
