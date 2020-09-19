e5e91ed56a930f3969080b21b8c448eec10d149f
Dylan Leifeste <ldylan01@gmail.com>
2020-09-18 22:33:49 -0500

    hard $%$ hackerrank problem

--- HackerRank/Arrays.js
+++ HackerRank/Arrays.js
@@ -70,3 +70,27 @@ function minimumSwaps(arr) {
 
     return minSwaps;
 }
+
+// Complete the arrayManipulation function below.
+function arrayManipulation(n, queries) {
+    const arr = Array(n + 1);
+    let maxValue = 0,
+        currentNumber = 0;
+    queries.forEach(([startRange, endRange, value]) => {
+        arr[startRange] = arr[startRange] || { start: 0, end: 0 };
+        arr[endRange] = arr[endRange] || { start: 0, end: 0 };
+        arr[startRange].start += value;
+        arr[endRange].end += value;
+    });
+    arr.forEach((cell) => {
+        if (cell) {
+            currentNumber += cell.start;
+            if (currentNumber > maxValue) {
+                maxValue = currentNumber;
+            }
+            currentNumber -= cell.end;
+        }
+    });
+    return maxValue;
+
+}
